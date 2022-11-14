ee3fa60cdedb581f56aadbfd099122dcd72f98a5
Andrew Nguyen <anguyen@ripl.org>
2022-11-14 10:56:29 -0700

    updating predict to treat classification and regression as separate tasks. fixed yhat generation bug

--- R/predict.R
+++ R/predict.R
@@ -18,17 +18,30 @@ predict.dml <- function(data
                         , y_var
                         , x_vars
                         , family = 'ols'
+                        , seed = 10272022
                         , ...){
-  x_data <- dplyr::select(data, x_vars)
+  x_data <- dplyr::select(data, dplyr::all_of(x_vars))
   y_data <- dplyr::pull(data, y_var)
+  mode <- 'regression'
+  prediction_type <- 'numeric'
 
   if(!family %in% c('ols', 'rf'))
     stop('argument "family" must be one of "ols" or "rf')
 
   if(is.logical(y_data)){
-    warning(paste0('Logical variable "', y_var, '" being converted to 1/0 numeric'))
-    y_data <- as.numeric(y_data)
+    warning(paste0('Logical variable "', y_var, '" being converted to factor'))
+    y_data <- as.factor(y_data)
+    mode <- 'classification'
+    prediction_type <- 'prob'
   }
+
+  if('character' %in% lapply(x_data, class)){
+    warning(paste0('Converting character columns to factor'))
+    x_data <- x_data %>%
+      dplyr::mutate_if(is.character, as.factor)
+  }
+
+  set.seed(seed)
 
   df <- x_data %>%
     dplyr::mutate(y = y_data)
@@ -52,54 +65,119 @@ predict.dml <- function(data
 
   #fit model family
   if(family == 'ols'){
-    model_spec_fold1 <- parsnip::linear_reg() %>%
-      parsnip::set_engine("lm")
+    if(mode == 'classification')
+      model_spec_fold1 <- parsnip::logistic_reg() %>%
+        parsnip::set_engine("glm") %>%
+        parsnip::set_mode(mode)
+
+    if(mode == 'regression')
+      model_spec_fold1 <- parsnip::linear_reg() %>%
+        parsnip::set_engine("lm") %>%
+        parsnip::set_mode(mode)
 
     model_spec_fold2 <- model_spec_fold1
 
   } else if(family == 'rf'){
 
     #perform cross validation on rf
-    model_spec_fold1 <- fit_cv_rf(fold_1, ...)
-    model_spec_fold2 <- fit_cv_rf(fold_2, ...)
+    model_spec_fold1 <- fit_cv_rf(fold_1, rf_mode = mode, ...)
+    model_spec_fold2 <- fit_cv_rf(fold_2, rf_mode = mode, ...)
 
   }
 
   #use the parameters of fold1 on fold2 data
   model_fit_fold1 <- parsnip::fit(model_spec_fold1, y ~ ., data = fold_1)
   model_fit_fold2 <- parsnip::fit(model_spec_fold2, y ~ ., data = fold_2)
+  if(mode == 'classification'){
+    predictions[fold2_indices] <- predict(model_fit_fold1, fold_2, type = prediction_type)$.pred_TRUE
+    predictions[fold1_indices] <- predict(model_fit_fold2, fold_1, type = prediction_type)$.pred_TRUE
 
-  predictions[fold2_indices] <- predict(model_fit_fold1, fold_2)$.pred
-  predictions[fold1_indices] <- predict(model_fit_fold2, fold_1)$.pred
+    resids[fold2_indices] <- as.logical(y_data[fold2_indices]) - predictions[fold2_indices]
+    resids[fold1_indices] <- as.logical(y_data[fold1_indices]) - predictions[fold1_indices]
+  }else{
 
-  resids[fold2_indices] <- predictions[fold2_indices] - y_data[fold2_indices]
-  resids[fold1_indices] <- predictions[fold1_indices] - y_data[fold1_indices]
+    predictions[fold2_indices] <- predict(model_fit_fold1, fold_2, type = prediction_type)$.pred
+    predictions[fold1_indices] <- predict(model_fit_fold2, fold_1, type = prediction_type)$.pred
+
+    resids[fold2_indices] <- y_data[fold2_indices] - predictions[fold2_indices]
+    resids[fold1_indices] <- y_data[fold1_indices] - predictions[fold1_indices]
+  }
+
+
 
   #save model specs and residuals
   out <- list(model_fit_fold1 = model_fit_fold1
               , model_fit_fold2 = model_fit_fold2
               , predictions = predictions
-              , resids = resids)
+              , resids = resids
+              , data = df)
 
   out
 }
 
 fit_cv_rf <- function(data
-                      , grid_size = 20
-                      , fold_size = 4
-                      , use_default_hyper = TRUE){
+                      , rf_mode = "regression"
+                      , use_default_hyper = TRUE
+                      , trees = NULL
+                      , min_n = NULL){
+
 
   model_spec_default <- parsnip::rand_forest() %>%
-    parsnip::set_mode("regression") %>%
+    parsnip::set_mode(mode = rf_mode) %>%
     parsnip::set_engine("ranger")
 
-  if(use_default_hyper)
+  if(use_default_hyper & is.null(trees) & is.null(min_n)){
+    message("Using default random forest hyperparameters")
     return(model_spec_default)
+  }
+
+  if(!is.null(trees)&!is.null(min_n)){
+    message("Using user supplied random forest hyperparameters")
+    model_spec <- parsnip::rand_forest(trees = trees, min_n = min_n) %>%
+      parsnip::set_mode(mode = rf_mode) %>%
+      parsnip::set_engine("ranger")
+
+    return(model_spec)
+  }
+
+
+  model_spec_tune <- parsnip::rand_forest(trees = tune(), min_n = tune()) %>%
+    parsnip::set_mode(mode = rf_mode) %>%
+    parsnip::set_engine("ranger")
+
+  if(!is.null(trees))
+    model_spec_tune  <- model_spec_tune %>% set_args(trees = trees)
+  if(!is.null(min_n))
+    model_spec_tune  <- model_spec_tune %>% update(min_n = min_n)
+
 
   message("Tuning random forest hyperparameters")
-  model_spec_tune <- parsnip::rand_forest(trees = tune(), min_n = tune()) %>%
-    parsnip::set_mode("regression") %>%
+
+
+  best_hyper_parameters <- cross_validate(data, model_spec_tune, rf_mode)
+
+  model_spec <- parsnip::rand_forest() %>%
+    parsnip::set_mode(mode = rf_mode) %>%
     parsnip::set_engine("ranger")
+
+  if(is.null(trees))
+    model_spec <- model_spec %>% set_args(trees = best_hyper_parameters$trees)
+  else
+    model_spec <- model_spec %>% set_args(trees = trees)
+
+  if(is.null(min_n))
+    model_spec <- model_spec %>% set_args(min_n = best_hyper_parameters$min_n)
+  else
+    model_spec <- model_spec %>% set_args(min_n = min_n)
+
+  model_spec
+}
+
+cross_validate <- function(data
+                           , model_spec_tune
+                           , mode
+                           , grid_size = 4
+                           , fold_size = 4){
 
 
   rf_cv <- rsample::vfold_cv(data, v = fold_size)
@@ -110,17 +188,16 @@ fit_cv_rf <- function(data
   #cl <- parallel::makePSOCKcluster(all_cores)
   #doParallel::registerDoParallel(cl)
 
-  rf_res <- tune::tune_grid(object = model_spec_tune
-                            , preprocessor = dml_recipe
-                            , resamples = rf_cv
-                            , grid = grid_size
-                            , control = control_grid(verbose = TRUE))
+  tuned_models <- tune::tune_grid(object = model_spec_tune
+                                  , preprocessor = dml_recipe
+                                  , resamples = rf_cv
+                                  , grid = grid_size
+                                  , control = tune::control_grid(verbose = TRUE))
+  metric <- 'rmse'
+  if(mode == 'regression')
+    metric <- 'rmse'
+  if(mode == 'classification')
+    metric <- 'accuracy'
 
-  best_hyper_parameters <- select_best(rf_res, metric = "rmse")
-
-  model_spec <- parsnip::rand_forest(trees = best_hyper_parameters$trees, min_n = best_hyper_parameters$min_n) %>%
-    parsnip::set_mode("regression") %>%
-    parsnip::set_engine("ranger")
-
-  model_spec
+  best_hyper_parameters <- tune::select_best(tuned_models, metric = metric)
 }
