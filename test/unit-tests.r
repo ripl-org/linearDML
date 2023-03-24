153d98de7f168cf732e32f9eab1945bb7db1c959
Andrew Nguyen <anguyen@ripl.org>
2023-03-24 07:08:48 -0600

    merging unit_tests.R and examples.R

--- test/unit-tests.r
+++ test/unit-tests.r
@@ -1,5 +1,8 @@
 library(linearDML)
-
+library(causaldata)
+library(dplyr)
+library(tidyr)
+library(neuralnet)
 
 test_dml.lm_y_var <- function(){
   data(iris)
@@ -91,6 +94,64 @@ test_dml.lm_h_vars <- function(){
   print('test_dml.lm_h_vars test completed')
 }
 
+test_dml.hyper_parameters <- function(){
+  df <- nsw_mixtape %>%
+    as.data.frame() %>%
+    dplyr::mutate(treat =  as.logical(treat))
+
+
+  #use random forest with two hyperparameters set
+  dml.lm(data = df
+         , x_vars = c('age', 'black', 'hisp', 'marr', 're74', 're75')
+         , y_var = 're78'
+         , d_vars = c('treat')
+         , first_stage_family = 'rf'
+         , second_stage_family = 'mr'
+         , mtry = 3
+         , trees = 100)
+
+  #use random forest with all hyperparameters set
+  dml.lm(data = df
+         , x_vars = c('age', 'black', 'hisp', 'marr', 're74', 're75')
+         , y_var = 're78'
+         , d_vars = c('treat')
+         , first_stage_family = 'rf'
+         , second_stage_family = 'mr'
+         , mtry = 3
+         , trees = 100
+         , max_depth = 2
+         , min_n = 2)
+}
+
+test_dml.user_function <- function(){
+
+  df <- nsw_mixtape %>%
+    as.data.frame() %>%
+    dplyr::mutate(treat =  as.logical(treat))
+
+  #use a user defined prediction function
+  nnet_predict_fun <- function(data, y_var, x_vars){
+    y <- data %>% pull(y_var)
+
+    formula <- as.formula(paste(y_var, paste(x_vars, collapse=" + "), sep=" ~ "))
+
+    model <- neuralnet::neuralnet(formula, data)
+    y_hat <- predict(model, newdata = data)
+
+    resids <- y - y_hat
+
+    list(resids = resids)
+  }
+
+
+  dml.lm(data = df
+         , x_vars = c('age', 'black', 'hisp', 'marr', 're74', 're75')
+         , y_var = 're78'
+         , d_vars = c('treat')
+         , first_stage_family = 'user-defined'
+         , predict_fun = nnet_predict_fun)
+
+}
 
 #======================================================
 # RUN ALL TESTS
@@ -102,10 +163,8 @@ test_dml.lm_d_vars()
 test_dml.lm_first_stage_family()
 test_dml.lm_second_stage_family()
 test_dml.lm_h_vars()
-
-
-
-
+test_dml.hyper_parameters()
+test_dml.user_function()
 
 
 
